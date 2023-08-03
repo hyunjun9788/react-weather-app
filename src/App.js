306db43aa21154f8453bc4606108e96534d54611
hyunjun9788 <namhyunjun9788@gmail.com>
2023-08-03 19:28:02 +0900

    feat: input 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import styled from 'styled-components'
 function App() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Daejeon&appid=72eef875e6f055e59edb083deaea23e2
+`
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrap>
+        <div className='appContentWrap'>
+            <input placeholder='도시를 입력하세요'/>
+        </div>
+    </AppWrap>
   );
 }
 
 export default App;
+
+const AppWrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  border: 1px red solid;
+
+  .appContentWrap {
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    position: absolute;
+    border: 1px blue solid;
+    padding: 20px;
+  }
+`
+
